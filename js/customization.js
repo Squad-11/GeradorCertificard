e0c9ee556f724f1ed5c8c586b38a70ff454dd38e
Matheus Carvalho <matheus10277@gmail.com>
2021-05-05 14:29:28 -0300

    Consertando regex

--- js/customization.js
+++ js/customization.js
@@ -76,7 +76,7 @@ function renderMediaIcons(){
 }
 
 function validateLink(link){
-  const regex = new RegExp("https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)")
+  const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
   return regex.test(link);
 }
 
